5e5c966c38c1a2d69a091399a77788251d8d2f57
Attila Kiss <mentoloskolbasz@gmail.com>
2020-06-19 23:22:43 +0200

    Kliens tesztelés Grafikus felület létrehozása és tesztelése

---
+++ MagusLib/MagusKliens/KarGen.cs
@@ -0,0 +1,27 @@
+﻿using MagusKliens.Kiszolgalo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagusKliens
+{
+    public partial class KarGen : Form
+    {
+        public KarGen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var kommunikator = new MagusSzerverClient();
+            textBox1.Text = kommunikator.GetData(15);
+        }
+    }
+}
