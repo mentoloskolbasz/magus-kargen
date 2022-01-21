78b7a650b1e05f86a553d70f5b4cd00ae4054cf9
Kiss Attila <mentoloskolbasz@gmail.com>
2022-01-21 21:45:40 +0100

    Szenvedés a PSZI-vel

---
+++ MagusLib/MagusLib/Kepzettsegek/IPszi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagusLib.Kepzettsegek
+{
+    public interface IPszi : IKepzettseg
+    {
+
+    }
+}
---
+++ MagusLib/MagusLib/PsziSzint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MagusLib.Kepzettsegek;
+using MagusLib.Kepzettsegek.Elasjatitott;
+
+
+namespace MagusLib
+{
+    public class PsziSzint
+    {
+        private IPszi aktualisIskola;
+        private LinkedList<IPszi> psziSzintek = new LinkedList<IPszi>();
+
+        public void SzintLepes()
+        {
+            
+        }
+        public void SzintVisszaLepes(int szintek = 1)
+        {
+
+        }
+
+        public IPszi AktualisIskola
+        {
+            get
+            {
+                return aktualisIskola;
+            }
+            set
+            {
+                aktualisIskola = value;
+            }
+        }
+    }
+}
